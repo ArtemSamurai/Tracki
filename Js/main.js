5da5cda9374edf9c578a1cd8c7f57a0d1c3f623b
Artemsamurai <modealohadance9@gmail.com>
2025-10-10 17:30:28 +0300

    small fixes

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
   const obj = document.getElementById("main");
-
-  obj.addEventListener("click", () => {
-    obj.classList.toggle("grow");
-  });
 });

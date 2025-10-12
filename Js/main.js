2081450a297d936d549260fa5e131ff8834ebd48
Artemsamurai <modealohadance9@gmail.com>
2025-10-12 11:19:57 +0300

    big upd

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const obj = document.getElementById("main");
+  const obj_1 = document.getElementById("Close")
+  const obj = document.getElementById("Open");
+  const target = document.getElementById("main_1")
+  obj.addEventListener("click", () => {
+    target.classList.toggle("open");
+  });
+  obj_1.addEventListener("click", () => {
+    target.classList.toggle("open");
+  });
+  target.addEventListener("animationend", () => {
+    if(target.classList.contains("open")){
+        obj.style.opacity = "0";
+    };
+  });
 });

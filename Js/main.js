70801d286dde02d3abd3de2fa1900276e724d42e
Artemsamurai <modealohadance9@gmail.com>
2025-10-13 22:08:02 +0300

    add button what changes language

--- Js/main.js
+++ Js/main.js
@@ -1,16 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const obj_1 = document.getElementById("Close")
+  let langarr = {
+    "title": {
+        "ru": "Любимые треки",
+        "ua": "Улюблені треки",
+        "en": "Songs what i like",
+    },
+    "header": {
+        "ru": "Это сайт про песни которые мне нравятся",
+        "ua": "Це сайт про пісні які мені подобаються",
+        "en": "This web-site about song what i like",
+    },
+    "like": {
+        "ru": "🎵Тут находятся песни которые мне нравятся🎹",
+        "ua": "🎵Тут знаходяться треки які мені подобаються🎹",
+        "en": "🎵Here contains songs what i like🎹",
+    },
+    "like_1": {
+        "ru": "🎵Тут находятся песни которые мне нравятся🎹",
+        "ua": "🎵Тут знаходяться треки які мені подобаються🎹",
+        "en": "🎵Here contains songs what i like🎹",
+    },
+    "link": {
+        "ru": "Как со мной связаться",
+        "ua": "Як зі мною зв'язатися",
+        "en": "How to contact with me",
+    }
+  }
+  const allLang = ["ru", "ua", "en"];
+  const obj_1 = document.getElementById("Close");
   const obj = document.getElementById("Open");
-  const target = document.getElementById("main_1")
+  const select = document.querySelector("#lang");
+  const target = document.getElementById("main_1");
   obj.addEventListener("click", () => {
     target.classList.toggle("open");
   });
   obj_1.addEventListener("click", () => {
     target.classList.toggle("open");
   });
-  target.addEventListener("animationend", () => {
-    if(target.classList.contains("open")){
-        obj.style.opacity = "0";
-    };
-  });
+  select.addEventListener("change", changeUrllang);
+
+  function changeUrllang() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+  };
+
+  function changelanguange() {
+    let hash = window.location.hash
+    hash = hash.substring(1)
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + "#en";
+        location.reload();
+    }
+    select.value = hash;
+    for(let key in langarr) {
+        document.querySelector("#lang-" + key).innerHTML = langarr[key][hash]; 
+    }
+  }
+  changelanguange();
+
+
 });
